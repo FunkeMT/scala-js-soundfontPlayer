f30a4fa16a94e2e7c76bafc6217b2d7d96264136
Markus Funke <funkemarkus@me.com>
2018-06-28 18:10:57 +0200

    workaround for JS-AudioContext in Safari browser

--- src/main/scala/soundfontPlayer/Soundfont.scala
+++ src/main/scala/soundfontPlayer/Soundfont.scala
@@ -6,7 +6,19 @@ import scala.scalajs.js
 
 @js.native
 object Soundfont extends js.Object {
-  def instrument(ac: AudioContext, name: String, options: js.Object = null): js.Promise[SamplePlayer] = js.native
+  /**
+    * Creates a new Soundfont instrument.
+    *
+    *
+    * @param ac 'Object with Dynamic' Actually, the type is 'AudioContext.
+    *           But Safari works only with 'webkitAudioContext'.
+    *           However, to apply this argument for all browsers you can pass:
+    *           js.Dynamic.newInstance(js.Dynamic.global.AudioContext || js.Dynamic.global.webkitAudioContext)()
+    * @param name String The Instrument name
+    * @param options js.Object Optional arguments
+    * @return js.Promise[SamplePlayer] JavaScript Promise with type 'SamplePlayer'
+    */
+  def instrument(ac: Object with Dynamic, name: String, options: js.Object = null): js.Promise[SamplePlayer] = js.native
 
   def nameToUrl(name: String, sf: String, format: String): String = js.native
 }
